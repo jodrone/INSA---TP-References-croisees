c0f92ae9223c7793953485e6bdf721b19da621ff
Tristan Pourcelot <tristan.pourcelot@gmail.com>
2011-11-14 14:42:52 +0100

    Début de gestion des arguments

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,16 +2,20 @@
                            Main  - Programme Principal
                              -------------------
     début                : ...
-    copyright            : (C) 2011 par Tristan Pourcelot & Jordan Vincent
+copyright            : (C) 2011 par Tristan Pourcelot & Jordan Vincent
 *************************************************************************/
 
 //---------- Réalisation de la tâche <Main> (fichier Main.cpp) ---
 
 /////////////////////////////////////////////////////////////////  INCLUDE
 //-------------------------------------------------------- Include système
+#include <iostream>
+#include <string>
 
+using namespace std;
 //------------------------------------------------------ Include personnel
 #include "Main.h"
+
 
 ///////////////////////////////////////////////////////////////////  PRIVE
 //------------------------------------------------------------- Constantes
@@ -34,13 +38,38 @@
 //////////////////////////////////////////////////////////////////  PUBLIC
 //---------------------------------------------------- Fonctions publiques
 int main ( int argc,const char* argv[] )
-// Algorithme :
-//
+// Algorithme : Traitement trivial des arguments
+// En cas d'erreur, on renvoie un message d'erreur + Usage()
 {
+  switch ( argc )
+	{
+		case 1 :
+		  Usage( "Erreur, liste d'arguments vide" );
+		  break;
+		case 2 :
+		  Usage( "Erreur : ");
+		  break;
+
+		default :
+		Usage ( "Erreur generale" );
+		
+	
+	}
+
+
+
 } //----- fin de Nom
 
 
-void Usage(string Phrase)
+void Usage ( string aPhrase )
 //Algorithme : Trivial
 {
+	cout << aPhrase << endl;
+	cout << "Usage : refCroisees [ -k fichier_keyword ] -e fichier1  ... fichier_n " << endl;
+	cout << "Usage : refCroisees [ -k fichier_keyword ] fichier1 ... fichier_n" << endl;
+	cout << "Options : " << endl;
+	cout << " -k : permet d'indiquer le fichier de mots cles a utiliser" << endl;
+	cout << "Si -k est absent, les mots cles du C++ sont utilises par defaut " << endl;
+	cout << " -e : permet d'exclure les mots clés " << endl;
+	cout << " License DWTFYWPL. Copyleft 2011 par T.Pourcelot & J.Vincent" << endl;
 } //----- fin de Usage
