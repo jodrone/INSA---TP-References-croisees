0e948a8ad9ff5001aae9eb728f0c2eb53aba4849
Tristan Pourcelot <tristan.pourcelot@gmail.com>
2011-12-05 11:58:27 +0100

    Microsoft & accent => SH*T

--- src/Fichier.h
+++ src/Fichier.h
@@ -1,7 +1,7 @@
 /*************************************************************************
                            Fichier  -  description
                              -------------------
-    dï¿½but                : ...
+    debut                : Nov. 2011
     copyright            : (C) 2011 par Tristan Pourcelot & Jordan Vincent 
 *************************************************************************/
 
@@ -9,7 +9,7 @@
 #if ! defined ( FICHIER_H )
 #define FICHIER_H
 
-//--------------------------------------------------- Interfaces utilisï¿½es
+//--------------------------------------------------- Interfaces utilisees
 #include <vector>
 #include <string>
 //------------------------------------------------------------- Constantes 
@@ -17,10 +17,10 @@
 //------------------------------------------------------------------ Types 
 
 //------------------------------------------------------------------------ 
-// Rï¿½le de la classe <Fichier>
+// Role de la classe <Fichier>
 // 
-// La classe Fichier a pour rï¿½le de reprï¿½senter les occurences d'un fichier 
-// pour un identificateur donnï¿½.
+// La classe Fichier a pour role de representer les occurences d'un fichier 
+// pour un identificateur donne.
 //
 //------------------------------------------------------------------------ 
 
@@ -29,70 +29,57 @@ class Fichier
 //----------------------------------------------------------------- PUBLIC
 
 public:
-//----------------------------------------------------- Mï¿½thodes publiques
+//----------------------------------------------------- Methodes publiques
 	void DisplayFichier() const;
 	// Mode d'emploi : Affiche sur la sortie standard la liste des numeros
 	// de lignes associes au fichier.
 	//
 
 	bool operator==(Fichier const & unFichier) const;
-	// Mode d'emploi : renvoie vrai unFichier est ï¿½gal ï¿½ this
+	// Mode d'emploi : renvoie vrai unFichier est egal ou this
 	//
 	// Contrat : le test ne ce fait que sur l'attribut nomFic !
 	//
 
 	bool AddLigne( int numLigne );
-	// Mode d'emploi : Ajoute un numï¿½ro de ligne au vecteur de numï¿½ros de 
-	// lignes. Incrï¿½mente nbNumLignes. 
-	// Renvoie faux si numï¿½ro dï¿½ja existant, vrai sinon.
+	// Mode d'emploi : Ajoute un numero de ligne au vecteur de numeros
+	// de lignes. Incremente nbNumLignes. 
+	// Renvoie faux si numero deja existant, vrai sinon.
 	//
 	// Contrat : numLigne est strictement positif.
-	//			 les numï¿½ros sont supposï¿½s triï¿½s par ordre croissant et
-	// numLigne est supposï¿½ ï¿½tre plus grand que tous les numï¿½ros du vector
-	//
+	//		les numeros sont supposes tries par ordre croissant
+	//		et numLigne est suppose etre plus grand que tous les
+	//		numeros du vector
 
-  	// Fonction();
-	// Mode d'emploi :
-	//
-	// Contrat : 
-	//
 
 //-------------------------------------------- Constructeurs - destructeur
 
 	Fichier( const Fichier & unFichier );
 	// Mode d'emploi (constructeur de copie) : construit une copie de 
 	// unFichier
-	//
 	// Contrat :
-	//
 
 	Fichier ( string unNom );
 	// Mode d'emploi : constructeur 
-	// Initialise nomFic et met numLignes à NULL
-	//
-	// Contrat :
-	//
+	// Initialise nomFic et met numLignes a NULL
 
     virtual ~Fichier ( );
 	// Mode d'emploi (destructeur) :
-	//
-	// Contrat :
-	//
 
 //------------------------------------------------------------------ PRIVE 
 protected:
-//----------------------------------------------------- Mï¿½thodes protï¿½gï¿½es	
+//----------------------------------------------------- Methodes protegees	
 	
-//----------------------------------------------------- Attributs protï¿½gï¿½s
+//----------------------------------------------------- Attributs proteges
 
 	string nomFic;
 	// nom du fichier
 
 	vector <int> *numLignes;
-	// Pointeur sur un tableau dynamique de numeros de lignes tries par ordre 
-	// croissant.
+	// Pointeur sur un tableau dynamique de numeros de lignes tries
+	// par ordre  croissant.
 };
 
-//----------------------- Autres dï¿½finitions dï¿½pendantes de <Fichier>
+//----------------------- Autres definitions dependantes de <Fichier>
 
 #endif // FICHIER_H
